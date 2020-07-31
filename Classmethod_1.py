46ee0e1a44885b494a7e90456813755b148053a0
YOONSXNG <yoonsungkr@kakao.com>
2020-07-31 22:11:35 +0900

    Class method example

---
+++ Classmethod_1.py
@@ -0,0 +1,21 @@
+class User:
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+    @classmethod
+    def from_string(cls, string_params):
+        string_list = string_params.split(",")
+        return User(string_list[0], string_list[1], string_list[2])
+
+    @classmethod
+    def from_list(cls, list_params):
+        return User(list_params[0], list_params[1], list_params[2])
+
+# 유저 생성 및 초기값 설정
+younghoon = User.from_string("강영훈,younghoon@codeit.kr,123456")
+yoonsoo = User.from_list(["이윤수", "yoonsoo@codeit.kr", "abcdef"])
+
+print(younghoon.name, younghoon.email, younghoon.password)
+print(yoonsoo.name, yoonsoo.email, yoonsoo.password)
